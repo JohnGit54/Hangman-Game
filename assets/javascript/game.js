352d665eea14c426cb60d6813aacf8b5d51fb6c6
JohnGit54 <johnp701@yahoo.com>
2018-03-14 20:31:22 -0400

    18-03-14 8:30pm

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,12 @@
 
 
 //global variables
-var isGameOver = false;
+var isGameOver = true;
+var guessnum = 10;
+var incorrectGuesses = 0
+var letterPressed = "";
+var wins = 0;
+var losses = 0;
 
 var alpray = [["A", 0], ["B", 0], ["C", 0],
 ["D", 0], ["E", 0], ["F", 0],
@@ -11,18 +16,114 @@ var alpray = [["A", 0], ["B", 0], ["C", 0],
 ["J", 0], ["K", 0], ["L", 0],
 ["M", 0], ["N", 0], ["O", 0],
 ["P", 0], ["Q", 0], ["R", 0],
-["S", 0], ["T", 0], ["U", 0],
+["S", 9990], ["T", 0], ["U", 0],
 ["V", 0], ["W", 0], ["X", 0],
 ["Y", 0], ["Z", 0]];
 
+var randomword = ["President", "longtime", "personal",
+    "aide", "fired", "investigation", "Secret",
+    "Service", "financial", "crimes"];
+
+var mysteryH = document.getElementById("mysteryWord");
+
+//end of globals
 
 
+//start functions
+function initializeGame() {
 
+    isGameOver = false;
+    var guessnum = 10;
+    var incorrectGuesses = 0
 
+    for (let i = 0; i < alpray.length; i++) {
+        alpray[i][1] = 0;
+    }
+
+    //pick random word
+    var randX = Math.floor(Math.random() * randomword.length);
+    var mysteryWord = randomword[randX];
+     console.log("mysteryWord: " + mysteryWord, randX);
+
+    //put blanks in panel
+    var st; 
+    var stDisplay = []; //this is used innerhtml panel
+    var stHidden = []; // this represents array - one for each letter
+    for (let index = 0; index < mysteryWord.length; index++) {
+        st = "&nbsp;&nbsp"   + "_" + "&nbsp;&nbsp"   ;
+        stDisplay.push(st)  ;
+         st = "&nbsp;&nbsp;"  + mysteryWord[index] + "&nbsp; &nbsp;"     ;
+         stHidden.push(st) ;
+    };
+    console.log("st: "+ stDisplay );
+    console.log("st: "+ stHidden );
+    st = "";
+    stDisplay.forEach(element => {
+        st += element        
+    });
+
+    document.getElementById("mysteryWord").innerHTML = st; //stDisplay;
+    document.getElementById("guessremain").innerHTML = guessnum;
+
+    //unhide all letters
+    // to be written later
+
+    //put pic 0 back
+    document.getElementById("myImage").src = "assets/images/0.jpg";
+
+}
+
+function blackoutLetter(myId) {
+    console.log("blackout", myId);
+    document.getElementById(myId).style.color = "red";
+    document.getElementById(myId).style.background = "red";
+}
+
+function checkLetter(ltr){
+    var ind = mysteryWord.findIndex;
+    console.log("checkltr",mysteryWord,ltr,);
+    // if ()
+}
 
 //start processing code
 document.onkeyup = function (event) {
-    alert("in javascript");
+
+    letterPressed = event;
+    console.log("event.keyCode", event.keyCode, "letterPressed.key", letterPressed.key);
+
+    //RESET GAME -- IF GAME IS OVER AND SPACEBAR IS PRESSED
+    if (event.keyCode === 32 && isGameOver) {
+        //Initialize all variables
+        initializeGame();
+        return;
+    }
+
+    //validate the key pressed - only accept Aplhabet -upper or lower allowed
+    if ((event.keyCode >= 65 && event.keyCode <= 90) || (event.keyCode >= 97 && event.keyCode <= 122)) {
+        console.log("after init rtne", letterPressed.key);
+        letterPressed = letterPressed.key.toUpperCase();
+    } else { return; }
+
+
+    //get the index in the aplhabet array of letter pressed 
+    // coolest line of code foundWOW
+    var indx = alpray.findIndex(sa => sa[0] === letterPressed);
+
+    console.log("indx", indx);
+    //check the inde- so we catch any unexplained err.
+    // black out the letter on screen box3
+    if (indx > -1) {
+        // console.log(alpray[indx][1]);
+        if (alpray[indx][1] === 0) {
+            alpray[indx][1] += 1;
+            blackoutLetter(letterPressed + 3);
+            checkLetter(letterPressed);
+        }
+
+    } else { return; }
+
+
+
 
 
 
